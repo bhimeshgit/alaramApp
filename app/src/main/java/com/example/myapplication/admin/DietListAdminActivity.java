182979f16d8e2a2332dafc93488975a889ebb65a
tacktile <sarang.daware@tacktile.in>
2023-03-30 17:58:38 +0530

    added repository class

--- app/src/main/java/com/example/myapplication/admin/DietListAdminActivity.java
+++ app/src/main/java/com/example/myapplication/admin/DietListAdminActivity.java
@@ -9,16 +9,10 @@ import com.example.myapplication.R;
 
 public class DietListAdminActivity extends AppCompatActivity {
 
-    private RecyclerView dietListRecyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diet_list_admin);
-        generateId();
     }
-
-    private void generateId() {
-        dietListRecyclerView = findViewById(R.id.dietListRecyclerView);
-
-    }
+    
 }
